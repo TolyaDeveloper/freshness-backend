80a164a9a273c03fd9727bb924453613a8249683
Anatolii <49586997+TolyaDeveloper@users.noreply.github.com>
2022-09-01 12:45:25 +0300

    Feature/find category by (#16)
    * find category by id
    
    * rollback

--- src/modules/shop/interfaces/shop.repository.interface.ts
+++ src/modules/shop/interfaces/shop.repository.interface.ts
@@ -1,14 +1,17 @@
-import mongoose from 'mongoose'
 import { type CategoryModelType } from '../../../models/category.model'
 import { ProductModelType } from '../../../models/product.model'
 import { TagModelType } from '../../../models/tag.model'
 import { CategoryDto } from '../dto/category.dto'
 import { ProductDto, IFindProductsQueries } from '../dto/product.dto'
 import { TagDto } from '../dto/tag.dto'
+import mongoose from 'mongoose'
 
 export interface IShopRepository {
   findAllCategories: () => Promise<CategoryModelType[]>
   addCategory: (category: CategoryDto) => Promise<CategoryModelType>
+  findCategoryById: (
+    id: mongoose.Types.ObjectId
+  ) => Promise<CategoryModelType | null>
   createProduct: (product: ProductDto) => Promise<ProductModelType>
   findProducts: (queries: IFindProductsQueries) => Promise<ProductModelType[]>
   findProductById: (
